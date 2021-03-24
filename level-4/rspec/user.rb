f5dad822a306c19eb7a7a3e95b8d2300c819e370
Alexandre de Castro Trindade <alexandredct@gmail.com>
2021-03-24 11:04:13 -0300

    Using the "let" method

---
+++ level-4/rspec/user.rb
@@ -0,0 +1,14 @@
+class User
+  def initialize(name, birthday)
+    @name = name
+    @birthday = birthday
+  end
+
+  def name
+    @name
+  end
+
+  def born_in_leap_year?
+    leap_year?(Date.parse(@birthday).year)
+  end
+end
---
+++ level-4/rspec/user_spec-2.rb
@@ -0,0 +1,39 @@
+require "date"
+require "./user"
+
+def leap_year?(year)
+  year % 400 == 0 or year % 100 != 0 and year % 4 == 0
+end
+
+describe User do
+  let(:user) { User.new("Francisca", "#{year}-01-01") }
+
+  context "born in 2001" do
+    let(:year) { 2001 }
+    it "is not born in a leap year" do
+      expect(user).not_to be_born_in_leap_year
+    end
+  end
+
+  context "born in 1900" do
+    let(:year) { 1900 }
+    specify "is not born in a leap year" do
+      expect(user).not_to be_born_in_leap_year
+    end
+  end
+
+  context "born in 2000" do
+    let(:year) { 2000 }
+    example "is born in a leap year" do
+      expect(user).to be_born_in_leap_year
+    end
+  end
+
+  context "born in 2004" do
+    let(:year) { 2004 }
+    it "is born in a leap year" do
+      user = User.new("Francisca", "2004-01-01")
+      expect(user).to be_born_in_leap_year
+    end
+  end
+end
